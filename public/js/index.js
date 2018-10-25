d0c4f9c610182e22934df5a8f2676251f038c7a7
github username <github email address>
2018-10-25 14:37:23 +0100

    Add form for messages and show inoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -4,10 +4,27 @@ socket.on('connect', function () {
   console.log('Connected to server');
 
   socket.on('newMessage', function (message) {
-    console.log(message);
+
+    let li = jQuery('<li> </li>');
+    li.text(`${message.from}: ${message.text}`);
+
+    jQuery('#messages').append(li);
   });
 });
 
+
 socket.on('disconnect', function () {
   console.log('Disconnected from the  server');
+});
+
+jQuery('#message-form').on('submit', function(e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+
+  }, function() {
+
+  });
 });
